907e8a0eed0b4f53c7bc0f33daf7789a7ce4c9a9
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-08-11 23:47:39 +0300

    stille better listing

--- lua/ai/aiconfig.lua
+++ lua/ai/aiconfig.lua
@@ -43,16 +43,18 @@ function aiconfig.listScannedFilesAsText()
   local analyzed_files_as_string = ""
   if 1 == #aiconfig.listScannedFilesFromConfig() then
     analyzed_files_as_string = "The project is composed of one file: " .. analyzed_files_as_array[1]
-  elseif 2 == #aiconfig.listScannedFilesFromConfig() then
+  end
+  if 2 == #aiconfig.listScannedFilesFromConfig() then
     analyzed_files_as_string = "The project is composed of two files: " .. analyzed_files_as_array[1] .. " and " .. analyzed_files_as_array[2]
-  else
+  end
+  if 3 <= #aiconfig.listScannedFilesFromConfig() then
     analyzed_files_as_string = "The project is composed of " .. #aiconfig.listScannedFilesFromConfig() .. " files: "
-    end
+  end
   for i, file in ipairs(analyzed_files_as_array) do
-    if i == #analyzed_files_as_array then
-      analyzed_files_as_string = analyzed_files_as_string .. "and " .. file
+    if i == (#analyzed_files_as_array - 1) then
+      analyzed_files_as_string = analyzed_files_as_string .. analyzed_files_as_array[#analyzed_files_as_array-1] .. "and " .. analyzed_files_as_array[#analyzed_files_as_array]
     else
-      analyzed_files_as_string = analyzed_files_as_string .. file .. ", "
+      analyzed_files_as_string = analyzed_files_as_string .. analyzed_files_as_array[i] .. ", "
     end
   end
 

5ab2ebdafc41752eb7ac5c6493313db06ba2400b
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-09-22 22:23:26 +0300

    warn instead of error

--- lua/ai/gemini/query.lua
+++ lua/ai/gemini/query.lua
@@ -73,7 +73,7 @@ function query.askHeavy(instruction, prompt, opts, agent_host)
   table.insert(body_chunks, {role = 'user',  content = aiconfig.listScannedFilesAsText()})
   for _, context in pairs(project_context) do
     if aiconfig.contentOf(context) == nil then
-      error("The content of `" .. context .. "` is nil.")
+      warn("The content of `" .. context .. "` is nil.")
     else
       table.insert(body_chunks, {role = 'model', content = "What is the content of `" .. context .. "` ?"})
       table.insert(body_chunks, {role = 'user',  content = "The content of `" .. context .. "` is :\n```\n" .. aiconfig.contentOf(context) .. "\n```"})

844f92b23b35e62d16942fcccc5316d4df38fe47
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-11-26 15:31:25 +0300

    intercept

--- lua/ai/init.lua
+++ lua/ai/init.lua
@@ -157,13 +157,24 @@ function M.handle(name, input)
     update(M.fill(def.result_tpl or '${output}', args)) -- Update the popup directly
   end
   local number_of_files = #aiconfig.listScannedFilesFromConfig()
-  log("Gemini agent URL is " .. M.opts.gemini_agent_host " and ChatGPT agent URL is " .. M.opts.chatgpt_agent_host " and number of files is " .. number_of_files)
+
+  -- If M.opts.gemini_agent_host is not set, set it to an empty string
+  if M.opts.gemini_agent_host == nil then
+    M.opts.gemini_agent_host = ''
+  end
+
+
+  if M.opts.chatgpt_agent_host == nil then
+    M.opts.chatgpt_agent_host = ''
+  end
+
+  M.log("Gemini agent URL is " .. M.opts.gemini_agent_host " and ChatGPT agent URL is " .. M.opts.chatgpt_agent_host " and number of files is " .. number_of_files)
   if ((M.opts.gemini_agent_host == '' or M.opts.chatgpt_agent_host == '') or number_of_files == 0) then
-    log("Not using agents")
+    M.log("Not using agents")
     gemini.ask(instruction, prompt,{handleResult = function(gemini_output)  return handleResult(gemini_output,  'gemini_output')  end,callback = function() end},M.opts.gemini_api_key)
     chatgpt.ask(instruction,prompt,{handleResult = function(chatgpt_output) return handleResult(chatgpt_output, 'chatgpt_output') end,callback = function() end},M.opts.chatgpt_api_key)
   else
-    log("Using agents")
+    M.log("Using agents")
     gemini.askHeavy(instruction,prompt, {handleResult = function(gemini_output)  return handleResult(gemini_output,  'gemini_output')  end,callback = function() end},M.opts.gemini_agent_host)
     chatgpt.askHeavy(instruction,prompt,{handleResult = function(chatgpt_output) return handleResult(chatgpt_output, 'chatgpt_output') end,callback = function() end},M.opts.chatgpt_agent_host)
   end

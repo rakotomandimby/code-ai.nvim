434900b5d857f65526c7e2bb73bee150af612318
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-12-20 01:34:27 +0300

    no pcall fisrt..

--- lua/ai/history.lua
+++ lua/ai/history.lua
@@ -4,32 +4,28 @@ local common = require('ai.common')
 
 -- Create the '.ai-history' directory under the project root if it doesn't exist
 function history.createHistoryDir()
-    local historyDir = aiconfig.getProjectRoot() .. '/.ai-history'
-    local historyDirExists = vim.fn.isdirectory(historyDir)
-    if not historyDirExists then
-        vim.fn.mkdir(historyDir, 'p')
-    end
+  local historyDir = aiconfig.getProjectRoot() .. '/.ai-history'
+  local historyDirExists = vim.fn.isdirectory(historyDir)
+  if not historyDirExists then
+    vim.fn.mkdir(historyDir, 'p')
+  end
 end
 
 function history.saveToHistory(model, content)
-    -- handle errors and log them
-    if not pcall(history.createHistoryDir) then
-        common.log("Error: Failed to create history directory")
-        return
-    end
-    local timestamp = os.date("%Y-%m-%d_%H-%M-%S")
-    local fileName = model .. '_' .. timestamp .. ".md"
-    local filePath = aiconfig.getProjectRoot() .. '/.ai-history/' .. fileName
-    local file = io.open(filePath, "w")
-    if file then
-        file:write(content)
-        file:close()
-        return filePath
-    else
-        return nil
-    end
+  common.log("Saving history to " .. model .. " history file")
+  history.createHistoryDir()
+  common.log("Creating history file for " .. model)
+  local timestamp = os.date("%Y-%m-%d_%H-%M-%S")
+  local fileName = model .. '_' .. timestamp .. ".md"
+  local filePath = aiconfig.getProjectRoot() .. '/.ai-history/' .. fileName
+  local file = io.open(filePath, "w")
+  if file then
+    file:write(content)
+    file:close()
+    return filePath
+  else
+    return nil
+  end
 end
-
-
 
 return history

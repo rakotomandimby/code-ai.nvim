faa1595ff6212db1a178cad3b87ccc1bbee6f40c
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-12-20 01:41:52 +0300

    debug

--- lua/ai/history.lua
+++ lua/ai/history.lua
@@ -5,9 +5,11 @@ local common = require('ai.common')
 -- Create the '.ai-history' directory under the project root if it doesn't exist
 function history.createHistoryDir()
   local historyDir = aiconfig.getProjectRoot() .. '/.ai-history'
+  common.log("Checking if history directory exists: " .. historyDir)
   local historyDirExists = vim.fn.isdirectory(historyDir)
   if not historyDirExists then
     vim.fn.mkdir(historyDir, 'p')
+    common.log("Created history directory: " .. historyDir)
   end
 end
 
@@ -15,10 +17,13 @@ function history.saveToHistory(model, content)
   common.log("Saving history to " .. model .. " history file")
   history.createHistoryDir()
   common.log("Creating history file for " .. model)
-  local timestamp = os.date("%Y-%m-%d_%H-%M-%S")
-  local fileName = model .. '_' .. timestamp .. ".md"
+  -- Generate a unique filename based on the model and timestamp go to milliseconds
+  -- to avoid collisions
+  -- Example: chatgpt_2023-03-15_10-30-00-0000.md
+  local fileName = model .. "_" .. os.date("%Y-%m-%d_%H-%M-%S") .. "-" .. string.format("%04d", os.time()) .. ".md"
   local filePath = aiconfig.getProjectRoot() .. '/.ai-history/' .. fileName
   local file = io.open(filePath, "w")
+  common.log("Writing to history file: " .. filePath)
   if file then
     file:write(content)
     file:close()

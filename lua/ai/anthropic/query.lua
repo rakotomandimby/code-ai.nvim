3131cf64ad524354081af4d6e3fdf6292261d663
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-03-17 12:48:42 +0300

    anthropic query file

---
+++ lua/ai/anthropic/query.lua
@@ -0,0 +1,113 @@
+local curl = require('plenary.curl')
+local aiconfig = require('ai.aiconfig')
+local common = require('ai.common')
+local query = {}
+local history = require('ai.history')
+
+local promptToSave = ""
+local modelUsed = ""
+
+function query.formatResult(data)
+  common.log("Inside Anthropic formatResult")
+  local prompt_tokens = data.usage.prompt_tokens
+  local completion_tokens = data.usage.completion_tokens
+
+  local formatted_prompt_tokens = string.format("%gk", math.floor(prompt_tokens / 1000))
+  local formatted_completion_tokens = string.format("%gk", math.floor(completion_tokens / 1000))
+
+  -- Create the result string with token counts
+  local result = '\n# This is '.. modelUsed .. ' answer (' .. formatted_prompt_tokens .. ' in, ' .. formatted_completion_tokens .. ' out)\n\n'
+  result = result .. data.choices[1].message.content .. '\n\n'
+  history.saveToHistory('claude_' .. modelUsed , promptToSave .. '\n\n' .. result)
+  return result
+end
+
+query.askCallback = function(res, opts)
+  common.askCallback(res, opts, query.formatResult)
+end
+
+function query.askHeavy(model, instruction, prompt, opts, agent_host)
+  promptToSave = prompt
+  modelUsed = model
+  local url = agent_host .. '/anthroppic]'
+  local project_context = aiconfig.listScannedFilesFromConfig()
+  local body_chunks = {}
+  table.insert(body_chunks, {system_instruction = instruction})
+  table.insert(body_chunks, {role = 'user', content = "I need your help on this project."})
+  table.insert(body_chunks, {role = 'model', content = "Tell me the project file structure."})
+  table.insert(body_chunks, {role = 'user',  content = aiconfig.listScannedFilesAsText()})
+  for _, context in pairs(project_context) do
+    if aiconfig.contentOf(context) ~= nil then
+      table.insert(body_chunks, {role = 'model', content = "What is the content of `" .. context .. "` ?"})
+      table.insert(body_chunks, {role = 'user',  content = "The content of `" .. context .. "` is :\n```\n" .. aiconfig.contentOf(context) .. "\n```"})
+    end
+  end
+  table.insert(body_chunks, {role = 'model', content = "Then what do you want me to do with all that information?"})
+  table.insert(body_chunks, {role = 'user', content = prompt})
+  table.insert(body_chunks, {model_to_use = model})
+  table.insert(body_chunks, {temperature = 0.2})
+  table.insert(body_chunks, {top_p = 0.1})
+  table.insert(body_chunks, {})
+
+  local function sendNextRequest(i)
+    if i > #body_chunks then
+      return
+    end
+
+    local message = body_chunks[i]
+    local body = vim.json.encode(message)
+
+    curl.post(url,
+      {
+        headers = {['Content-type'] = 'application/json'},
+        body = body,
+        callback = function(res)
+          if i == #body_chunks then
+            vim.schedule(function() query.askCallback(res, opts) end)
+          else
+            sendNextRequest(i + 1)
+          end
+        end
+      })
+  end
+
+  sendNextRequest(1)
+
+end
+
+
+function query.ask(model, instruction, prompt, opts, api_key)
+  promptToSave = prompt
+  modelUsed = model
+  local api_host = 'https://api.anthropic.com'
+  -- local api_host = 'https://eowloffrpvxwtqp.m.pipedream.net'
+  local path = '/v1/messages'
+  curl.post(api_host .. path,
+    {
+      headers = {
+        ['Content-type'] = 'application/json',
+        ['x-api-key'] = api_key,
+        ['anthropic-version'] = '2023-06-01'
+      },
+      body = vim.fn.json_encode(
+        {
+          model = model,
+          max_tokens = 8192,
+          system= instruction,
+          messages = (function()
+            local messages = {}
+            table.insert(messages, {role = 'user', content = prompt})
+            return messages
+          end)()
+        }
+      ),
+      callback = function(res)
+        common.log("Before Anthropic callback call")
+        vim.schedule(function() query.askCallback(res, opts) end)
+      end
+    })
+end
+
+return query
+
+
